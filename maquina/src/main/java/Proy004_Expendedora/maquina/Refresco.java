d661c334ceb6d33b4c0fe495761534adc57005e6
khrixthianru <61119680+khrixthianru@users.noreply.github.com>
2022-10-05 14:22:13 +0200

    practika  makina

---
+++ maquina/src/main/java/Proy004_Expendedora/maquina/Refresco.java
@@ -0,0 +1,53 @@
+package Proy004_Expendedora.maquina;
+
+public class Refresco {
+	//atrib
+	private String sabor; //idproducto
+	private int cl;
+	private double precio;
+	private int stock;
+	private boolean agotado;
+	
+	//constructores
+	public Refresco(String sabor, int cl, double precio, int stock, boolean agotado) {
+		super();
+		this.sabor = sabor;
+		this.cl = cl;
+		this.precio = precio;
+		this.stock = stock;
+		this.agotado = agotado;
+	}
+	//para modificar el precio
+	public Refresco(String sabor, double precio) {
+		this(sabor, 33, precio, 100, true);
+		this.sabor = sabor;
+		this.precio = precio;
+	}
+	//para modificar el stock
+	public Refresco(String sabor, int stock) {
+		this(sabor, 33, 2.0, stock, true);
+		this.sabor = sabor;
+		this.stock = stock;
+	}
+	
+	//setter and getter
+	public String getSabor() {
+		return sabor;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
