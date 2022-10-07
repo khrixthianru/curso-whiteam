f22765ba69f2e1ccb7c6732d93d21526201d5087
khrixthianru <61119680+khrixthianru@users.noreply.github.com>
2022-10-07 14:07:34 +0200

    gfsdgfsd

--- Proy006_TresEnRaya/src/main/java/com/curso/Juego/TresEnRaya.java
+++ Proy006_TresEnRaya/src/main/java/com/curso/Juego/TresEnRaya.java
@@ -1,12 +1,75 @@
 package com.curso.Juego;
+
+import javax.management.RuntimeErrorException;
+
 /**
  * Implementa el juego 3 en raya
  * @author alumno
  *
  */
 public class TresEnRaya {
+	
+	//atributos
+	private Character [][] tablero;
+	private Character jugadorActual;
+	
+	//constructor
+
+	public TresEnRaya() {
+		super();
+		this.tablero = new Character[3][3];//todas las letras tendran null
+		this.jugadorActual = 'X';
+	}
+	
+	//getter and setter
+	public Character getJugadorActual() {
+		return jugadorActual;
+	}
+	
+	//metodos
 
 	public int suma(int x, int y) {
 		return x + y;
 	}
+
+
+	public void jugar(int x, int y) {
+		
+		validarCoordenada(x);
+		
+		validarCoordenada(y);
+		
+		ponerFicha(x,y);		
+		
+	}
+	
+	public Character verFicha(int x, int y) {		
+		validarCoordenada(x);
+		validarCoordenada(y);
+		return this.tablero[x][y];
+	}
+	
+	private void validarCoordenada(int x) {
+		if(x<0 || x>2) {
+			throw new RuntimeException("La ficha no se puede posicionar "
+					+ "fuera del tablero. "
+					+ "Coordenada x mal.");
+		}
+	}
+	
+	private void ponerFicha(int x, int y) {
+		//valida que no hay ya una pieza en x,y
+		if(tablero[x][y] != null) {
+			throw new RuntimeException("ya hay un ficha en esta posicion");
+		}
+		
+		tablero[x][y]= this.jugadorActual;
+		
+		if(this.jugadorActual == 'X') {
+			this.jugadorActual = 'O';
+		}else {
+			this.jugadorActual = 'X';
+		}
+	}
+	
 }
--- Proy006_TresEnRaya/src/test/java/com/curso/Juego/TresEnRayaTest.java
+++ Proy006_TresEnRaya/src/test/java/com/curso/Juego/TresEnRayaTest.java
@@ -46,8 +46,81 @@ public class TresEnRayaTest {
 		if(espero != resul) {
 			fail("no sumado 2+2 = 4 sino que el resul " + resul);
 		}
+		
+	}
+	
+	@Test (expected = RuntimeException.class)
+	public void ponerFichaEnCoordenaXMalLanzaUnaExcepcion() {
+		TresEnRaya juego = new TresEnRaya();
+		juego.jugar(2,4); //falla si no lanza la exception
+			
+	}
+	
+
+	@Test(expected = RuntimeException.class)
+	public void ponerFichaEnCoordenaYMalLanzaUnaExcepcion() {
+		TresEnRaya juego = new TresEnRaya();
+		juego.jugar(2, 4); // falla si no lanza la exception
+
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void PonerFichaEnPosicionQyeYaHayOtraFichaLanzaExcepcion() {
+		//Preparar el escenario
+		TresEnRaya juego = new TresEnRaya();
+		juego.jugar(0, 0);
+		juego.jugar(0, 0);//debe lanzar excepcion
+	}
+	
+	@Test
+	public void ponerFichaXeYBienEsperaQueEnEstaPosicionYaNoSeaNul() {
+		TresEnRaya juego = new TresEnRaya();
+		juego.jugar(0, 0);
+		
+		if(juego.verFicha(0,0) == null) {
+			fail("La ficha debe estar en esta posición y no hay ficha");
+		}
+	}// fin requisito 1
+	
+	@Test
+	public void primerValorEsX() {
+		TresEnRaya juego = new TresEnRaya();
+		
+		if(juego.getJugadorActual() != 'X') {
+			fail("El primer jugador debe ser X ");
+		}
+	}
+	
+	@Test
+	public void siAnteriorJugadorEsXAhoraEsO() {
+		TresEnRaya juego = new TresEnRaya();
+		juego.jugar(0, 0);
+		
+		if(juego.getJugadorActual() != 'O') {
+			fail("El primer jugador debe ser O ");
+		}
+	}
+	
+	@Test
+	public void siAnteriorJugadorEsOAhoraEsX() {
+		TresEnRaya juego = new TresEnRaya();
+		juego.jugar(0, 0);
+		juego.jugar(0, 2);
+		
+		if(juego.getJugadorActual() != 'X') {
+			fail("El primer jugador debe ser X ");
+		}
+	}
+	
+	@Test
+	public void hayTresEnLinea() {
+		TresEnRaya juego = new TresEnRaya();
+		
 	}
 	
 	
-
+	
 }
+	
+	
+
