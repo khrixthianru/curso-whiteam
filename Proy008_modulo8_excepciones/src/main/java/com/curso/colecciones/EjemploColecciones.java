5ce393ec5342d77901b7b567779d2f1e37ceb9d1
khrixthianru <61119680+khrixthianru@users.noreply.github.com>
2022-10-14 09:56:58 +0200

    fdsg
    df

---
+++ Proy008_modulo8_excepciones/src/main/java/com/curso/colecciones/EjemploColecciones.java
@@ -0,0 +1,25 @@
+package com.curso.colecciones;
+
+public class EjemploColecciones {
+
+	public static void main(String[] args) {
+		
+		//tamaño ilimitado, solo admiten objetos, muchos tipos
+		/*
+		 * Collection es un interface
+		 * Set <<I>> alamacenan un valor
+		 * List <<I>>
+		 * Map <<I>> almacenan key - value
+		 * 
+		 * clase utilidad collections y arrays
+		 * 
+		 * 
+		 * 
+		 * collections genericas
+		 * 
+		 * comparable y comparator
+		 */
+		
+	}
+
+}
---
+++ Proy008_modulo8_excepciones/src/main/java/ej1_modulo8/gfhd.java
@@ -0,0 +1,10 @@
+package ej1_modulo8;
+
+public class gfhd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
